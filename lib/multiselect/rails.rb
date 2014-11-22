963c3554dce97e165a60b2b25af33283dedfaaf2
esshka <esshka@gmail.com>
2014-11-22 05:16:34 +0300

    first

---
+++ lib/multiselect/rails.rb
@@ -0,0 +1,8 @@
+require "multiselect/rails/version"
+
+module Multiselect
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
---
+++ lib/multiselect/rails/version.rb
@@ -0,0 +1,5 @@
+module Multiselect
+  module Rails
+    VERSION = "0.6"
+  end
+end
